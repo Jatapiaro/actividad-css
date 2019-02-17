e555dc0a9bb324692a216604f1cff1c971c31b09
Jacobo Tapia <jacob.et.cetera@gmail.com>
2019-02-16 23:38:11 -0600

    Init

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.css';
+
+import Wrapper from './components/Wrapper';
+
+ReactDOM.render(<Wrapper />, document.getElementById("app"));
---
+++ src/components/Wrapper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Wrapper extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Hola
+            </div>
+        ); 
+    }
+
+}
