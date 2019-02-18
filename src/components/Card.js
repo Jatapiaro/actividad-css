bd575bce280476c8fdb9aca91ca7c0ffe9f51309
Jacobo Tapia <jacob.et.cetera@gmail.com>
2019-02-17 20:51:45 -0600

    Añade css apra mobiles

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ export default class Card extends React.Component {
         return (
             <div className="card">
                 <div className="info-hover">
-                    <div className="title">Personajes del comic: </div>
+                    <div className="title"><b>Personajes del comic: </b></div>
                     <div className="characters">
                         {
                             this.props.comic.characters.map((c, i) =>
@@ -23,9 +23,25 @@ export default class Card extends React.Component {
                 <div style={{ backgroundImage: `url(${this.props.comic.images[0]})` }} className="image"/>
                 <div style={{ backgroundImage: `url(${this.props.comic.images[0]})` }} className="image-hover" />
                 <div className="info">
-                    <div className="title">{this.props.comic.title}</div>
+                    <div className="title"><b>{this.props.comic.title}</b></div>
                     <div className="description" 
                         dangerouslySetInnerHTML={{ __html: this.props.comic.description}}/>
+                    <div className="heroes-info">
+                        {
+                            this.props.comic.characters.length > 0 &&
+                            <div className="title">
+                                <b> Personajes del comic: </b>
+                            </div>
+                        }
+                        {
+                            this.props.comic.characters.length > 0 &&
+                            this.props.comic.characters.map((c, i) => 
+                                <p key={i} className="character-info">
+                                    <img src={c.thumbnail} key={i} /><b>{`${c.name}: `}</b>{`${c.description}`}
+                                </p>
+                            )
+                        }
+                    </div>
                 </div>
             </div>
         );
